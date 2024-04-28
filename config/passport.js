a2819f0ae159459f5ebed06c3386d4e411409335
stanley <stanely12301@gmail.com>
2024-04-28 22:45:50 +0800

    本地登入驗證完成

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const passport = require("passport");
-const GoogleStragety = require("passport-google-oauth20");
+const GoogleStrategy = require("passport-google-oauth20");
 const User = require("../models/user_model");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
 
 passport.serializeUser( (user, done) => {   // 這裡的done和下面use裡async的沒有關聯
   console.log("serializing user ...");
@@ -16,14 +18,14 @@ passport.deserializeUser( async (_id, done) => {
 })
 
 passport.use(
-  new GoogleStragety(
+  new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:8000/auth/google/redirect" // 拿到想要的資料後，要重新導向到的URL
     },
     async (accessToken, refreshToken, profile, done) => {
-      console.log("entering GoogleStragety");
+      console.log("entering GoogleStrategy");
       // console.log(profile);
       console.log("=========================================");
       let foundUser = await User.findOne({googleID: profile.id}).exec();
@@ -45,3 +47,17 @@ passport.use(
     }
   )
 );
+
+passport.use( new LocalStrategy(async (username, password, done) => {   // login.ejs登入時用的變數名稱(username, password)要跟這裡一樣才會自動套入
+  let foundUser = await User.findOne({email: username});
+  if (foundUser){
+    let result = await bcrypt.compare(password, foundUser.password);
+    if (result){
+      done(null, foundUser);  // 要放入foundUser，這樣serialize User時才能存取到資料(ID)
+    }else {
+      done(null, false);
+    }
+  }else {
+    done(null, false);  // done是從LocalStrategy那邊來的，第二個參數設為false表示驗證失敗
+  }
+}))
--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -4,19 +4,19 @@ const User = require("../models/user_model");
 const bcrypt = require("bcrypt");
 
 router.get("/login", (req, res) => {
-  return res.render("login", {user: req.user});
+  return res.render("login", { user: req.user });
 });
 
 router.get("/logout", (req, res) => {
-  req.logOut(err => {
+  req.logOut((err) => {
     if (err) return res.send(err);
     return res.redirect("/");
   });
 });
 
 router.get("/signup", (req, res) => {
-  return res.render("signup", {user: req.user});
-})
+  return res.render("signup", { user: req.user });
+});
 
 router.get(
   "/google",
@@ -28,27 +28,42 @@ router.get(
 );
 
 router.post("/signup", async (req, res) => {
-  let {name, email, password} = req.body;
-  if (password.length < 8){
+  let { name, email, password } = req.body;
+  if (password.length < 8) {
     req.flash("error_msg", "密碼長度過短，需要至少8個英數字");
     return res.redirect("/auth/signup");
   }
   // 先檢查信箱是否有被註冊過
-  let foundEmail = await User.findOne({email}).exec();
-  if (foundEmail){
-    req.flash("error_msg", "此信箱已註冊過，請使用其他信箱註冊；或直接使用信箱登入");
+  let foundEmail = await User.findOne({ email }).exec();
+  if (foundEmail) {
+    req.flash(
+      "error_msg",
+      "此信箱已註冊過，請使用其他信箱註冊；或直接使用信箱登入"
+    );
     return res.redirect("/auth/signup");
   }
   // 沒被使用過就可以儲存到DB
   let hashPassword = await bcrypt.hash(password, 11);
-  let newUser = new User({name, email, password: hashPassword});
+  let newUser = new User({ name, email, password: hashPassword });
   await newUser.save();
   req.flash("success_msg", "註冊成功，導轉至登入頁面");
   return res.redirect("/auth/login");
 });
 
-router.get("/google/redirect", passport.authenticate("google"), (req, res) => {   // 因為這個route要通過google驗證才能使用，所以要authenticate
-  console.log("entering redirect zone")
+router.post(
+  "/login",
+  passport.authenticate("local", {  // 使用local驗證的middleware
+    failureRedirect: "/auth/login",   // 登入失敗時，會重新導向的地方
+    failureFlash: "登入失敗，帳號或密碼不正確",   // 登入失敗會顯示的提示 (會被套用在index.js裡的res.locals.error)
+  }),
+  (req, res) => {
+    return res.redirect("/profile");  
+  }
+);
+
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  // 因為這個route要通過google驗證才能使用，所以要authenticate
+  console.log("entering redirect zone");
   return res.redirect("/profile");
 });
 
