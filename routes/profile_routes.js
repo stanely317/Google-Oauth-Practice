2e05b8c997138f011a2ac6ea5d275d6d02fa1b03
stanley <stanely12301@gmail.com>
2024-04-29 21:51:37 +0800

    完成，測試OK

--- routes/profile_routes.js
+++ routes/profile_routes.js
@@ -1,16 +1,34 @@
 const router = require("express").Router();
+const Post = require("../models/post_model");
 
 const authCheck = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        next();
-    }else{
-        return res.redirect("/auth/login");
-    }
-}
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    return res.redirect("/auth/login");
+  }
+};
 
-router.get("/", authCheck, (req, res) => {
-    console.log("now we are in profile route");
-    return res.render("profile", {user: req.user});     // req.user是從DeserializeUser()來的
-})
+router.get("/", authCheck, async (req, res) => {
+  let foundPost = await Post.find({ author: req.user._id });
+  // console.log("now we are in profile route");
+  return res.render("profile", { user: req.user, posts: foundPost }); // req.user是從DeserializeUser()來的
+});
 
-module.exports = router
+router.get("/post", authCheck, (req, res) => {
+  return res.render("post", { user: req.user });
+});
+
+router.post("/post", authCheck, async (req, res) => {
+  let { title, content } = req.body;
+  let newPost = new Post({ title, content, author: req.user._id });
+  try {
+    await newPost.save();
+    return res.redirect("/profile");
+  } catch (error) {
+    req.flash("error_msg", "標題與內容皆須填寫");
+    return res.redirect("/profile/post");
+  }
+});
+
+module.exports = router;
